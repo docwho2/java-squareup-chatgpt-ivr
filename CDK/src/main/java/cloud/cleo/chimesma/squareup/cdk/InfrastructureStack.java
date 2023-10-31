b10ec9fe2402d432b2061de99239dad7919bb32c
Steve Jensen <docwho2@gmail.com>
2023-10-31 03:06:05 -0500

    WF tweaks

--- CDK/src/main/java/cloud/cleo/chimesma/squareup/cdk/InfrastructureStack.java
+++ CDK/src/main/java/cloud/cleo/chimesma/squareup/cdk/InfrastructureStack.java
@@ -3,16 +3,10 @@ package cloud.cleo.chimesma.squareup.cdk;
 import software.amazon.awscdk.App;
 import software.amazon.awscdk.CfnOutput;
 import software.amazon.awscdk.CfnOutputProps;
-import software.amazon.awscdk.RemovalPolicy;
 import software.constructs.Construct;
 import software.amazon.awscdk.Stack;
 import software.amazon.awscdk.StackProps;
 import software.amazon.awscdk.services.lambda.Runtime;
-import software.amazon.awscdk.services.s3.Bucket;
-import software.amazon.awscdk.services.s3.BucketPolicy;
-import software.amazon.awscdk.services.s3.BucketPolicyProps;
-import software.amazon.awscdk.services.s3.BucketProps;
-import software.amazon.awscdk.services.s3.CfnBucketPolicyProps;
 import software.amazon.awscdk.services.sam.CfnFunction;
 import software.amazon.awscdk.services.sam.CfnFunctionProps;
 
@@ -29,12 +23,6 @@ public class InfrastructureStack extends Stack {
 
     public InfrastructureStack(final Construct parent, final String id, final StackProps props) {
         super(parent, id, props);
-
-        // S3 Bucket for prompts
-        Bucket s3 = new Bucket(this, "PromptBucket", BucketProps.builder()
-                .removalPolicy(RemovalPolicy.DESTROY)
-        .build());
-        
        
         
         
