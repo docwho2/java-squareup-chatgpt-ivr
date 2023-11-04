7494f740444a0f63c7f583e69c4c80d13f7d9b47
Steve Jensen <steve@cleo.cloud>
2023-11-04 07:18:23 -0500

    remove CDK

--- CDK/deploy.bash
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-ACCOUNT_ID=`aws sts get-caller-identity --query Account --output text`
-
-cdk deploy -c accountId=${ACCOUNT_ID} --all --concurrency=5 --require-approval=never
--- CDK/destroy.bash
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-ACCOUNT_ID=`aws sts get-caller-identity --query Account --output text`
-
-cdk destroy -c accountId=${ACCOUNT_ID} --all --concurrency=5 --force
