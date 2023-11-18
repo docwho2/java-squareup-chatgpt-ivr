3225c0849d9d38d1249ae2325c1bd87547be40d3
Steve Jensen <steve@cleo.cloud>
2023-11-18 08:59:01 -0600

    Delete All log groups on destroy

--- destroy.sh
+++ destroy.sh
@@ -1,10 +1,22 @@
 #!/bin/bash
+#
+# Delete and clean up all resources
+
 
 # Stack names and regions
 source config.sh
 
-
+# Exit immediately if a command exits with a non-zero status.
+set -e
 ACCOUNT_ID=$(aws sts get-caller-identity --query Account --output text)
+# As long above works, attempt all other commands
+set +e
+
+pushd "ChimeCDKProvision"
+echo
+echo "Calling Destroy on CDK Stack to remove all Chime Voice SDK resources"
+cdk destroy -c accountId=${ACCOUNT_ID} -c stackName=${CDK_STACK_NAME} -c regionEast=${regions[0]} -c regionWest=${regions[1]} --all --force
+popd
 
 # delete things in each region
 for region in "${regions[@]}"; do
@@ -16,28 +28,27 @@ sam delete --region ${region} --stack-name ${STACK_NAME} --no-prompts
 
 # Delete the API Keys that were pushed
 echo
-echo "Deleting API keys from SSM Parameter Store in region ${region}"
+echo "Deleting OPENAI_API_KEY and SQUARE_API_KEY API from SSM Parameter Store in region ${region}"
 aws ssm --region ${region} delete-parameters --names "/${STACK_NAME}/OPENAI_API_KEY" "/${STACK_NAME}/SQUARE_API_KEY" > /dev/null
 
-# Delete the Custom Resource Log groups because if CF deletes it, the final delete of Lambda re-creates it
-echo
-echo "Deleting Log Group /aws/lambda/${STACK_NAME}-PromptCreator  in region ${region}"
-aws logs delete-log-group --region ${region} --log-group-name "/aws/lambda/${STACK_NAME}-PromptCreator" > /dev/null
-echo
-echo "Deleting Log Group /aws/lambda/${STACK_NAME}-PromptCopier  in region ${region}"
-aws logs delete-log-group --region ${region} --log-group-name "/aws/lambda/${STACK_NAME}-PromptCopier" > /dev/null
 
+echo
+echo "Deleting Log Groups starting with /aws/lambda/${CDK_STACK_NAME} in region ${region}"
+declare -a LGS=($(aws logs describe-log-groups --region ${region} --log-group-name-prefix /aws/lambda/${CDK_STACK_NAME} --query logGroups[].logGroupName --output text))
+for logGroup in "${LGS[@]}"; do
+echo "  Delete Log group [${logGroup}]"
+aws logs delete-log-group --region ${region} --log-group-name "${logGroup}" > /dev/null
 done
 
-pushd "ChimeCDKProvision"
+
 echo
-echo "Calling Destroy on CDK Stack to remove all Chime Voice SDK resources"
-cdk destroy -c accountId=${ACCOUNT_ID} -c stackName=${CDK_STACK_NAME} -c regionEast=${regions[0]} -c regionWest=${regions[1]} --all --force
+echo "Deleting Log Groups starting with /aws/lambda/${STACK_NAME} in region ${region}"
+declare -a LGS=($(aws logs describe-log-groups --region ${region} --log-group-name-prefix /aws/lambda/${STACK_NAME} --query logGroups[].logGroupName --output text))
+for logGroup in "${LGS[@]}"; do
+echo "  Delete Log group [${logGroup}]"
+aws logs delete-log-group --region ${region} --log-group-name "${logGroup}" > /dev/null
+done
 
-wait
-
-
-
-
+done
 
 
