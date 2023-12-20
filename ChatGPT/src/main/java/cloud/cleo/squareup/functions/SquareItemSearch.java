e673ffb67d3a80d48fb709213a13495eac83f8bc
Steve Jensen <docwho2@gmail.com>
2023-12-20 04:17:45 -0600

    Async futures on all search tokens for item search

--- ChatGPT/src/main/java/cloud/cleo/squareup/functions/SquareItemSearch.java
+++ ChatGPT/src/main/java/cloud/cleo/squareup/functions/SquareItemSearch.java
@@ -11,6 +11,7 @@ import com.fasterxml.jackson.annotation.JsonPropertyDescription;
 import com.squareup.square.models.SearchCatalogItemsRequest;
 import com.squareup.square.models.SearchCatalogItemsResponse;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.CompletionException;
@@ -47,17 +48,20 @@ public class SquareItemSearch<Request> extends AbstractFunction {
     public Function<Request, Object> getExecutor() {
         return (var r) -> {
             try {
-                final List<String> tokens = reduceTokens(r.search_text);
+                // Generate a list of search tokens with every combination of the words
+                final List<String> tokens = allCombinations(r.search_text);
 
+                // List to hold all the search futures
                 final List<CompletableFuture<SearchCatalogItemsResponse>> futures = new ArrayList<>(tokens.size());
 
+                // Launch all the searches
                 for (final var token : tokens) {
                     futures.add(getSquareClient().getCatalogApi()
                             .searchCatalogItemsAsync(new SearchCatalogItemsRequest.Builder().textFilter(token).limit(5).build()));
-                    log.debug("Queing Search on [" + token + "]");
+                    log.debug("Launching Search on [{}]", token);
                 }
 
-                log.debug("Starting wait on " + futures.size() + " futures");
+                log.debug("Starting wait on {} futures",futures.size());
                 CompletableFuture.allOf(futures.toArray(CompletableFuture[]::new)).join();
                 log.debug("All Futures completed, processing results");
 
@@ -65,7 +69,7 @@ public class SquareItemSearch<Request> extends AbstractFunction {
                 final List<String> itemNames = new ArrayList<>(5);
 
                 // Process the results
-                for (var future : futures) {
+                for (final var future : futures) {
                     final var items = future.getNow(null).getItems();
                     if (items != null && !items.isEmpty()) {
                         items.stream()
@@ -100,22 +104,30 @@ public class SquareItemSearch<Request> extends AbstractFunction {
         };
     }
 
-    public static List<String> reduceTokens(String input) {
-        List<String> reducedStrings = new ArrayList<>();
+    /**
+     * Given a String with several words, return all combinations of that in specific order for passing to searches.
+     *
+     * @param input
+     * @return
+     */
+    public static List<String> allCombinations(String input) {
         String[] tokens = input.split(" ");
 
-        for (int i = tokens.length; i > 0; i--) {
-            StringBuilder sb = new StringBuilder();
-            for (int j = 0; j < i; j++) {
-                sb.append(tokens[j]);
-                if (j < i - 1) {
-                    sb.append(" "); // Add space between words
-                }
+        // Generate combinations of the tokens
+        List<String> combinations = new ArrayList<>();
+
+        // Start with the full search term
+        combinations.add(input);
+
+        // Generate combinations from longest to shortest
+        for (int length = tokens.length - 1; length > 0; length--) {
+            for (int start = 0; start + length <= tokens.length; start++) {
+                String combination = String.join(" ", Arrays.copyOfRange(tokens, start, start + length));
+                combinations.add(combination);
             }
-            reducedStrings.add(sb.toString());
         }
 
-        return reducedStrings;
+        return combinations;
     }
 
     private static class Request {
