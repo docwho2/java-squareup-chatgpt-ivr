06a40ecd79f271d0b3afc85e300ca672221adba4
Steve Jensen <docwho2@gmail.com>
2023-12-27 06:06:00 -0600

    try square customer record lookup before sending email and attach

--- ChatGPT/src/main/java/cloud/cleo/squareup/functions/SendEmail.java
+++ ChatGPT/src/main/java/cloud/cleo/squareup/functions/SendEmail.java
@@ -4,9 +4,15 @@ import static cloud.cleo.squareup.ChatGPTLambda.crtAsyncHttpClient;
 import cloud.cleo.squareup.FaceBookOperations;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonPropertyDescription;
+import com.squareup.square.models.Customer;
+import com.squareup.square.models.CustomerFilter;
+import com.squareup.square.models.CustomerQuery;
+import com.squareup.square.models.CustomerTextFilter;
+import com.squareup.square.models.SearchCustomersRequest;
 import java.util.concurrent.CompletionException;
 import java.util.function.Function;
 import software.amazon.awssdk.services.ses.SesAsyncClient;
+import software.amazon.awssdk.services.ses.model.SendEmailRequest;
 
 /**
  * Send an Email message
@@ -44,6 +50,32 @@ public class SendEmail<Request> extends AbstractFunction {
         return (var r) -> {
 
             try {
+                String email = null;
+                Customer customer = null;
+
+                // If we have a valid phone number, try and look up customer in Square
+                if (hasValidUSE164Number()) {
+                    try {
+                        final var customerList = getSquareClient().getCustomersApi().searchCustomers(new SearchCustomersRequest.Builder()
+                                .query(new CustomerQuery.Builder()
+                                        .filter(new CustomerFilter.Builder()
+                                                .phoneNumber(new CustomerTextFilter.Builder().exact(getCallingNumber()).build())
+                                                .build())
+                                        .build())
+                                .limit(1L) // Only request one match, very unlikely more than one match
+                                .build()).getCustomers();
+                        if (!customerList.isEmpty()) {
+                            customer = customerList.get(0);
+                            if (customer.getEmailAddress() != null && !customer.getEmailAddress().isBlank()) {
+                                email = customer.getEmailAddress();
+                            }
+                        }
+                    } catch (Exception e) {
+                        // Just log error here, we don't want to stop email send
+                        log.error("Error in Customer lookup", e);
+                    }
+                }
+
                 // Put the callingNumber in the subject if it exists, it might not if using lex console for example
                 final String subject = switch (getChannelPlatform()) {
                     case CHIME, CONNECT ->
@@ -56,14 +88,25 @@ public class SendEmail<Request> extends AbstractFunction {
                         "[From " + getChannelPlatform() + "/" + getSessionId() + "] " + r.subject;
                 };
 
-                final var id = sesAsyncClient.sendEmail((email) -> {
-                    email.destination(dest -> dest.toAddresses(r.employee_email))
-                            .message((mesg) -> {
-                                mesg.body((body) -> {
-                                    body.text(cont -> cont.data(r.message));
-                                }).subject(cont -> cont.data(subject));
-                            }).source("chatgpt@copperfoxgifts.com");
-                }).join();
+                if (customer != null) {
+                    // Append Square Customer record to email for reference
+                    r.message = r.message.concat("\n\n").concat(mapper.valueToTree(customer).toPrettyString());
+                }
+
+                final var requestB = SendEmailRequest.builder()
+                        .destination(dest -> dest.toAddresses(r.employee_email))
+                        .message((mesg) -> {
+                            mesg.body((body) -> {
+                                body.text(cont -> cont.data(r.message));
+                            }).subject(cont -> cont.data(subject));
+                        }).source("CopperBot@CopperFoxGifts.com");
+
+                if (email != null) {
+                    // If we know the customers email, then set the reply to
+                    requestB.replyToAddresses(email);
+                }
+
+                final var id = sesAsyncClient.sendEmail(requestB.build()).join();
 
                 log.info("Sent email to " + r.employee_email + " with id " + id.messageId());
                 log.info("Subject: " + subject);
