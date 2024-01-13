42a24dbef6cb7f299728cae581a574b346bd8181
Steve Jensen <docwho2@gmail.com>
2024-01-13 07:49:40 -0600

    use OkHttp for Facebook operations

--- ChatGPT/src/main/java/cloud/cleo/squareup/FaceBookOperations.java
+++ ChatGPT/src/main/java/cloud/cleo/squareup/FaceBookOperations.java
@@ -1,17 +1,20 @@
 package cloud.cleo.squareup;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
 import lombok.NonNull;
+import okhttp3.MediaType;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 /**
- * Perform various Facebook operations. Used when Channel is FB.  Rather
- * than pull in some other dependency, we will just use basic HTTP for all
- * Facebook operations.
+ * Perform various Facebook operations. Used when Channel is FB. Rather than pull in some other dependency, we will just
+ * use basic HTTP for all Facebook operations.
  *
  * @author sjensen
  */
@@ -22,53 +25,55 @@ public class FaceBookOperations {
 
     private final static ObjectMapper mapper = new ObjectMapper();
 
+    private final static OkHttpClient httpClient = new OkHttpClient();
+
     /**
-     * Transfer control of Messenger Thread Session from Bot control to the
-     * Inbox. Used when end user needs to deal with a real person to resolve
-     * issue the Bot can't handle.  Some people despise Bots, so we need to allow
-     * getting the Bot out of the conversation.
-     * 
+     * Transfer control of Messenger Thread Session from Bot control to the Inbox. Used when end user needs to deal with
+     * a real person to resolve issue the Bot can't handle. Some people despise Bots, so we need to allow getting the
+     * Bot out of the conversation.
+     *
      * https://developers.facebook.com/docs/messenger-platform/handover-protocol/conversation-control
      *
      * @param id
      */
     public static void transferToInbox(String id) {
-        HttpURLConnection connection = null;
         try {
-            connection = (HttpURLConnection) getFaceBookURL(System.getenv("FB_PAGE_ID"), "pass_thread_control").openConnection();
-            connection.setRequestMethod("POST");
-            connection.setRequestProperty("Content-Type", "application/json; utf-8");
-            connection.setRequestProperty("Accept", "application/json");
-            connection.setDoOutput(true);
-
             // Construct the payload
-            var json = mapper.createObjectNode();
+            final var json = mapper.createObjectNode();
             // Special Target for Inbox
             json.put("target_app_id", "263902037430900");
             // The page scoped user ID of the person chatting with us
             json.putObject("recipient").put("id", id);
 
-            log.debug("Post Payload for thread control " + json.toPrettyString());
-            mapper.writeValue(connection.getOutputStream(), json);
+            // Create the request body
+            RequestBody body = RequestBody.create(json.toString(), MediaType.get("application/json; charset=utf-8"));
 
-            final int responseCode = connection.getResponseCode();
-            log.debug("Facebook Call Response Code: " + responseCode);
+            // Build the POST request
+            Request request = new Request.Builder()
+                    .url(getFaceBookURL(System.getenv("FB_PAGE_ID"), "pass_thread_control"))
+                    .post(body)
+                    .addHeader("Accept", "application/json")
+                    .build();
 
-            final var result = mapper.readTree(connection.getInputStream());
-            log.debug("FB Pass Thread Control result is " + result.toPrettyString());
+            // Execute the request and retrieve the response
+            try (Response response = httpClient.newCall(request).execute()) {
+                if (!response.isSuccessful()) {
+                    throw new Exception("Unexpected code " + response);
+                }
 
-            if (result.findValue("success") != null && result.findValue("success").asBoolean() == true) {
-                log.debug("Call Succeeded in passing thread control");
-            } else {
-                log.debug("Call FAILED to pass thread control");
+                // Parse the response as needed
+                final var result = mapper.readTree(response.message().getBytes());
+                log.debug("FB Pass Thread Control result is " + result.toPrettyString());
+
+                if (result.findValue("success") != null && result.findValue("success").asBoolean()) {
+                    log.debug("Call Succeeded in passing thread control");
+                } else {
+                    log.debug("Call FAILED to pass thread control");
+                }
             }
 
         } catch (Exception e) {
             log.error("Facebook Pass Thread Control error", e);
-        } finally {
-            if (connection != null) {
-                connection.disconnect();
-            }
         }
     }
 
@@ -79,42 +84,43 @@ public class FaceBookOperations {
      * @return
      */
     public static String getFacebookName(String id) {
-        HttpURLConnection connection = null;
         try {
-            connection = (HttpURLConnection) getFaceBookURL(id, null).openConnection();
-            connection.setRequestMethod("GET");
-            connection.setRequestProperty("Accept", "application/json");
+            // Build the GET request
+            Request request = new Request.Builder()
+                    .url(getFaceBookURL(id, null))
+                    .addHeader("Accept", "application/json")
+                    .build();
 
-            final int responseCode = connection.getResponseCode();
-            log.debug("Facebook Call Response Code: " + responseCode);
+            // Execute the request and get the response
+            try (Response response = httpClient.newCall(request).execute()) {
+                // Check if the response is successful
+                if (!response.isSuccessful()) {
+                    throw new Exception("Unexpected code " + response);
+                }
 
-            final var result = mapper.readTree(connection.getInputStream());
-            log.debug("FB Graph Query result is " + result.toPrettyString());
+                // Parse the JSON response
+                final var result = mapper.readTree(response.message().getBytes());
+                log.debug("FB Graph Query result is " + result.toPrettyString());
 
-            // Check for name first
-            if (result.findValue("name") != null) {
-                return result.findValue("name").asText();
+                // Check for name first
+                if (result.findValue("name") != null) {
+                    return result.findValue("name").asText();
+                }
+
+                // Usually returns first and last
+                if (result.findValue("first_name") != null && result.findValue("last_name") != null) {
+                    return result.findValue("first_name").asText() + " " + result.findValue("last_name").asText();
+                }
             }
-
-            // Usually returns first and last
-            if (result.findValue("first_name") != null && result.findValue("last_name") != null) {
-                return result.findValue("first_name").asText() + " " + result.findValue("last_name").asText();
-            }
-
         } catch (Exception e) {
             log.error("Facebook user name retrieval error", e);
-        } finally {
-            if (connection != null) {
-                connection.disconnect();
-            }
         }
 
         return "Unknown";
     }
 
     /**
-     * Get the base URL for Facebook Graph Operations with page access token
-     * incorporated.
+     * Get the base URL for Facebook Graph Operations with page access token incorporated.
      *
      * @param id
      * @param operation
